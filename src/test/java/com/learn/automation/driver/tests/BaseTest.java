764f15328fc4b58db42236314dcbc7fa9d43d01b
PankajKoriya30 <pankaj.koriya@outlook.com>
2026-09-22 11:47:02 +0530

    Day 10 - TestNG parameter

--- src/test/java/com/learn/automation/driver/tests/BaseTest.java
+++ src/test/java/com/learn/automation/driver/tests/BaseTest.java
@@ -6,17 +6,20 @@ import com.learn.automation.utils.TestDataReader;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
+    @Parameters("browser")
     @BeforeMethod(alwaysRun = true)
-    public void setup(){
+    public void setup(String browser){
 
         ConfigReader.loadProperties("qa");
         TestDataReader.loadProperties("testdata");
-        driver = DriverFactory.getDriver(ConfigReader.getProperty("browser"));
+        driver = DriverFactory.getDriver(browser);
         driver.get(ConfigReader.getProperty("url"));
     }
 
